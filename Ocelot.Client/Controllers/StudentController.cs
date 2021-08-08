1e762b3611a2df9424a87f620d276903aa67235d
akhmadjonjurayev <shunchakiadres@gmail.com>
2021-08-08 12:26:17 +0500

    first commit

---
+++ Ocelot.Client/Controllers/StudentController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Ocelot.Client.Controllers
+{
+    [Route("api/[controller]/[action]")]
+    [ApiController]
+    public class StudentController : ControllerBase
+    {
+        [HttpGet("{id:int}")]
+        public IActionResult GetStudentById(int id)
+        {
+            return Ok(new { name = "Shakhrizoda Jurayeva" });
+        }
+    }
+}
